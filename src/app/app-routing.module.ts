0c25b6eff330fa343b844c04d266c775da4db300
anurag-kokil-tor-ai <anurag.kokil@tor.ai>
2025-07-22 13:22:56 +0530

    Added service, configured HttpClientModule, and enabled CORS in ASP.NET Core Web API for API consumption

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  { path: 'books', component: BookListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Book } from '../models/book.model';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+}
